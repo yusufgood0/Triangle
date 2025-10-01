c22a1e21214691f2b08ed7f07d943e1e15b2ec0e
Yusuf SCHOOL <363120981@gotvdsb.ca>
2025-10-01 09:44:52 -0400

    continued working on enemies

--- Triangle/Enemies/Enemy.cs
+++ Triangle/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using random_generation_in_a_pixel_grid;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,26 @@ namespace Triangle.Enemies
 {
     internal interface Enemy
     {
-        public void Update(in Player player, in Random rnd);
+        public void Update(in Player player, in Random rnd, ref BoundingBox[] collisionObject, SeedMapper seedMap, int MapCellSize);
         public void EnemyHitPlayer(ref Player player);
         public void EnemyIsHit(ref Player player);
+
+        public static bool CheckLineOfSight(Vector3 Pos1, Vector3 Pos2, ref BoundingBox[] collisionObject)
+        {
+            float distance = Vector3.Distance(Pos1, Pos2);
+            foreach (var box in collisionObject)
+            {
+                Ray ray = new Ray(Pos2, Pos2 - Pos1);
+                if (box.Intersects(ray) < distance)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         public BoundingBox BoundingBox { get; }
         public BoundingBox Hitbox { get; }
         public Model[] models { get; }
+        public Vector3 Position { get; }
     }
 }
